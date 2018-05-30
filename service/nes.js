85c0c62e992416bf64ff2975fdeb1323c155a4af
Wafiq Baugh <screenlantern@gmail.com>
2018-05-30 14:12:53 +0100

    updates

--- service/nes.js
+++ service/nes.js
@@ -1,6 +1,6 @@
 import nes from "nes/client";
-const client = new nes.Client("ws://45.77.90.193:80");
-
+const client = new nes.Client("ws://localhost:8000"); //change on deployment server
+ 
 const NesClient = (async function() {
   try {
     await client.connect();
